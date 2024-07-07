f8f17697a41aea1e1933c340d378a9bd6b56b1be
MikeSoft007 <mekpenyong2@gmail.com>
2024-07-07 15:33:58 +0200

    Updated app for CORS

--- app/__init__.py
+++ app/__init__.py
@@ -2,23 +2,29 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from config import config  # Ensure this line imports the config correctly
+from flask_cors import CORS
 
 db = SQLAlchemy()
 jwt = JWTManager()
 
 def create_app(config_name):
     app = Flask(__name__)
+
+    # Load configuration from the config object
     app.config.from_object(config[config_name])
+
+    # Enable Cross-Origin Resource Sharing (CORS) for the app
+    CORS(app)
     
+    # Initialize extensions
     db.init_app(app)
     jwt.init_app(app)
 
     # Register Blueprints
-    from .views import auth_bp, user_bp, org_bp, user_home_bp
+    from .views import auth_bp, user_bp, org_bp, user_home_bp  # Ensure these imports are correct
     app.register_blueprint(auth_bp)
     app.register_blueprint(user_bp)
     app.register_blueprint(org_bp)
     app.register_blueprint(user_home_bp)
-
 
     return app
